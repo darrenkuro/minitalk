8170b69e41b96262d5204f3ccd1f362f03a7a948
darrenkuro <darrenkuro@icloud.com>
2023-05-18 17:16:13 +0200

    fix norm

--- submitted/minitalk.h
+++ submitted/minitalk.h
@@ -1,42 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/17 15:02:23 by dlu               #+#    #+#             */
-/*   Updated: 2023/05/18 17:13:52 by dlu              ###   ########.fr       */
+/*   Created: 2023/05/18 17:02:17 by dlu               #+#    #+#             */
+/*   Updated: 2023/05/18 17:06:38 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <signal.h>
-#include "libft.h"
-
-void	send_char(int pid, char c)
+#ifndef MINITALK_H
+# define MINITALK_H
+/*
+typedef struct s_sigqueue
 {
-	int	bit;
-
-	bit = 0;
-	while (bit < 8)
-	{
-		if (c & (0x01 << bit))
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		++bit;
-	}
-}
-
-int	main(int ac, char **av)
-{
-	int	i;
-
-	if (ac != 3)
-		return (1);
-	i = -1;
-	while (av[2][++i])
-		send_char(ft_atoi(av[1]), av[2][i]);
-	send_char(ft_atoi(av[1]), '\n');
-	return (0);
-}
+	t
+}	t_sigqueue;
+*/
+#endif
--- submitted/server.c
+++ submitted/server.c
@@ -6,19 +6,20 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 14:44:46 by dlu               #+#    #+#             */
-/*   Updated: 2023/05/18 15:20:41 by dlu              ###   ########.fr       */
+/*   Updated: 2023/05/18 17:14:03 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
+#include "libft.h"
 #include <signal.h>
+
+#define START_MSG "Server is now running. PID: %d\n"
 
 void	signal_handler(int signal)
 {
-	static int bit;
-	static int c;
-	
+	static int	bit;
+	static int	c;
+
 	if (signal == SIGUSR1)
 		c |= 1 << bit;
 	++bit;
@@ -32,9 +33,9 @@ void	signal_handler(int signal)
 
 int	main(void)
 {
+	ft_printf(START_MSG, getpid());
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
-	printf("Server Running. PID is: %d\n", getpid());
 	while (1)
 		pause();
 }
