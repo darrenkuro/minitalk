e4e0aa45200507fc1f21b603a26a19990f5f43fa
darrenkuro <darrenkuro@icloud.com>
2023-05-19 19:56:43 +0200

    style cleanup and norm

--- submitted/client.c
+++ submitted/client.c
@@ -6,30 +6,36 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 15:02:23 by dlu               #+#    #+#             */
-/*   Updated: 2023/05/19 18:00:29 by dlu              ###   ########.fr       */
+/*   Updated: 2023/05/19 19:56:24 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include "libft.h"
 
-#define SERVER_ERROR "Server is busy. Try again later.\n"
+#define SERVER_BUSY_MSG		"\033[31mServer is busy. Try again later.\n"
+#define SERVER_ERROR_MSG	"\033[31mServer can't be reached. Check PID.\n"
+#define PID_ERROR_MSG		"\033[31mPID consists of non-digit chars!\n"
+#define ARGS_ERROR_MSG		"\033[31mWrong number of "
+#define SUCCESS_MSG			"\033[34mMessage sent.\n"
+#define DELAY_MILLISEC		100
+#define TIMEOUT_LIMIT		10
 
 static int	g_received = 0;
 
-void	signal_handler(int signal)
+static void	signal_handler(int signal)
 {
-	usleep(50);
+	usleep(DELAY_MILLISEC);
 	if (signal == SIGUSR1)
 		g_received = 1;
 	if (signal == SIGUSR2)
 	{
-		write(1, SERVER_ERROR, 33);
+		write(1, SERVER_BUSY_MSG, 38);
 		exit(1);
 	}
 }
 
-void	send_char(int pid, char c)
+static void	send_char(int pid, char c)
 {
 	int	bit;
 
@@ -43,23 +49,54 @@ void	send_char(int pid, char c)
 			kill(pid, SIGUSR2);
 		++bit;
 		while (!g_received)
-			usleep(10);
+			usleep(DELAY_MILLISEC);
 	}
 	g_received = 0;
 }
 
-int	main(int ac, char **av)
+static int	pid_isvalid(char *s)
 {
 	int	i;
 
-	if (ac != 3)
-		return (1);
 	i = -1;
+	while (s[++i])
+		if (!ft_isdigit(s[i]))
+			return (0);
+	return (1);
+}
+
+static int	establish_connection(int pid)
+{
+	int	i;
+
+	i = -1;
+	kill(pid, SIGUSR1);
+	while (++i < TIMEOUT_LIMIT && !g_received)
+		usleep(DELAY_MILLISEC);
+	if (g_received)
+		return (1);
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	int	pid;
+	int	i;
+
+	if (ac != 3 && ft_printf("%s", ARGS_ERROR_MSG))
+		return (1);
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
+	if (!pid_isvalid(av[1]) && ft_printf("%s", PID_ERROR_MSG))
+		return (1);
+	pid = (int) ft_atoi(av[1]);
+	if (!establish_connection(pid) && ft_printf("%s", SERVER_ERROR_MSG))
+		return (1);
+	i = -1;
 	while (av[2][++i])
-		send_char(ft_atoi(av[1]), av[2][i]);
-	send_char(ft_atoi(av[1]), '\n');
-	send_char(ft_atoi(av[1]), '\0');
+		send_char(pid, av[2][i]);
+	send_char(pid, '\n');
+	send_char(pid, '\0');
+	ft_printf("%s", SUCCESS_MSG);
 	return (0);
 }
--- submitted/server.c
+++ submitted/server.c
@@ -6,39 +6,46 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 14:44:46 by dlu               #+#    #+#             */
-/*   Updated: 2023/05/19 17:58:28 by dlu              ###   ########.fr       */
+/*   Updated: 2023/05/19 19:55:53 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <signal.h>
 
-#define START_MSG "Server is now running. PID: %d\n"
+#define START_MSG "\033[34mServer is now running. PID: %d\033[0m\n"
 
 static int	g_client_pid = 0;
 
-void	signal_handler(int signum, siginfo_t *info, void *context)
+static void	write_char(int *c, int *bit)
+{
+	if (*c)
+		write(1, c, 1);
+	*bit = 0;
+	if (!*c)
+		g_client_pid = 0;
+	*c = 0;
+}
+
+static void	signal_handler(int signum, siginfo_t *info, void *context)
 {
 	static int	bit = 0;
 	static int	c;
 
 	(void) context;
 	if (g_client_pid == 0)
+	{
 		g_client_pid = info->si_pid;
+		kill(info->si_pid, SIGUSR1);
+		return ;
+	}
 	if (g_client_pid == info->si_pid)
 	{
 		if (signum == SIGUSR1)
 			c |= 1 << bit;
 		++bit;
 		if (bit == 8)
-		{
-			if (c)
-				write(1, &c, 1);
-			bit = 0;
-			if (c == 0)
-				g_client_pid = 0;
-			c = 0;
-		}
+			write_char(&c, &bit);
 		kill(info->si_pid, SIGUSR1);
 	}
 	else
@@ -56,4 +63,5 @@ int	main(void)
 	ft_printf(START_MSG, getpid());
 	while (1)
 		pause();
+	return (0);
 }
