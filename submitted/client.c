5646f7759ebdc6c3d995dd66377594953018dc6f
darrenkuro <darrenkuro@icloud.com>
2023-05-19 17:30:37 +0200

    init semi-working version

--- submitted/client.c
+++ submitted/client.c
@@ -6,12 +6,24 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 15:02:23 by dlu               #+#    #+#             */
-/*   Updated: 2023/05/18 17:13:52 by dlu              ###   ########.fr       */
+/*   Updated: 2023/05/19 17:22:39 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include "libft.h"
+
+static int received = 0;
+
+void	signal_handler(int signal)
+{
+	usleep(10);
+	if (signal == SIGUSR1)
+	{
+		received = 1;
+		//write(1, "r\n", 2);
+	}
+}
 
 void	send_char(int pid, char c)
 {
@@ -20,12 +32,16 @@ void	send_char(int pid, char c)
 	bit = 0;
 	while (bit < 8)
 	{
-		if (c & (0x01 << bit))
+		received = 0;
+		if (c & (1 << bit))
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
 		++bit;
+		while (!received)
+			usleep(50);
 	}
+	received = 0;
 }
 
 int	main(int ac, char **av)
@@ -35,8 +51,11 @@ int	main(int ac, char **av)
 	if (ac != 3)
 		return (1);
 	i = -1;
+	//ft_printf("PID: %d\n", getpid());
+	signal(SIGUSR1, signal_handler);
 	while (av[2][++i])
 		send_char(ft_atoi(av[1]), av[2][i]);
 	send_char(ft_atoi(av[1]), '\n');
+	send_char(ft_atoi(av[1]), '\0');
 	return (0);
 }
--- submitted/server.c
+++ submitted/server.c
@@ -6,7 +6,7 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 14:44:46 by dlu               #+#    #+#             */
-/*   Updated: 2023/05/18 17:14:03 by dlu              ###   ########.fr       */
+/*   Updated: 2023/05/19 17:27:31 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,42 @@
 
 #define START_MSG "Server is now running. PID: %d\n"
 
-void	signal_handler(int signal)
+void	signal_handler(int signum, siginfo_t *info, void *context)
 {
-	static int	bit;
+	static int	clientPID = 0;
+	static int	bit = 0;
 	static int	c;
 
-	if (signal == SIGUSR1)
-		c |= 1 << bit;
-	++bit;
-	if (bit == 8)
+	(void) context;
+	if (clientPID == 0)
+		clientPID = info->si_pid;
+	if (clientPID == info->si_pid)
 	{
-		write(1, &c, 1);
-		bit = 0;
-		c = 0;
+		if (signum == SIGUSR1)
+			c |= 1 << bit;
+		++bit;
+		kill(clientPID, SIGUSR1);
+		if (bit == 8)
+		{
+			if (c == 0)
+				clientPID = 0;
+			if (c)
+				write(1, &c, 1);
+			bit = 0;
+			c = 0;
+		}
 	}
 }
 
 int	main(void)
 {
+	struct sigaction	sa;
+
+	sa.sa_sigaction = signal_handler;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	ft_printf(START_MSG, getpid());
-	signal(SIGUSR1, signal_handler);
-	signal(SIGUSR2, signal_handler);
 	while (1)
 		pause();
 }
